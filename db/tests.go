f0b82f8cac56d4ba46ae05d47e4354d8b0ac8cbd
Maurizio Tomasi <maurizio.tomasi@unimi.it>
2017-06-01 08:33:21 +0200

    Make a few cosmetic changes to the source code

--- db/tests.go
+++ db/tests.go
@@ -23,63 +23,63 @@ package db
 import (
 	"fmt"
 	"io"
-	"path"
 	"os"
+	"path"
 	"time"
 
 	"github.com/astrogo/fitsio"
 
-	"github.com/lspestrip/stdb/convert"
 	"database/sql"
+	"github.com/lspestrip/stdb/convert"
 )
 
 // Test is a structure holding all the information related to a test
 type Test struct {
-	ShortName string // Short name of the test
-	Description string // Full description of the test
-	CreationDate time.Time // Time when the acquisition of data stopped
-	Username string // Name of the user which has uploaded the test in the db
-	FitsChecksum string // Checksum of the FITS file containing the test data
-	TestType string // Type of test (it should refer to the Test Plan document)
-	TimeSpanSec float64 // Length of the test, in seconds
-	CryogenicFlag bool // Was the test performed at cryogenic temperatures?
-	Polarimeter int // Number of the polarimeter being tested
-	NumOfSamples int // Number of samples acquired during the test
+	ShortName     string    // Short name of the test
+	Description   string    // Full description of the test
+	CreationDate  time.Time // Time when the acquisition of data stopped
+	Username      string    // Name of the user which has uploaded the test in the db
+	FitsChecksum  string    // Checksum of the FITS file containing the test data
+	TestType      string    // Type of test (it should refer to the Test Plan document)
+	TimeSpanSec   float64   // Length of the test, in seconds
+	CryogenicFlag bool      // Was the test performed at cryogenic temperatures?
+	Polarimeter   int       // Number of the polarimeter being tested
+	NumOfSamples  int       // Number of samples acquired during the test
 }
 
 // FileCopy copies the file with path "sourcePath" into the file with
 // path "destPath". No soft/hard links are used, and ownership and
 // permissions are not preserved.
 func FileCopy(destPath, sourcePath string) error {
-    in, err := os.Open(sourcePath)
-    if err != nil { 
+	in, err := os.Open(sourcePath)
+	if err != nil {
 		return err
 	}
-    defer in.Close()
+	defer in.Close()
 
-    out, err := os.Create(destPath)
-    if err != nil { 
+	out, err := os.Create(destPath)
+	if err != nil {
 		return err
 	}
-    defer out.Close()
+	defer out.Close()
 
-    _, err = io.Copy(out, in)
-    cerr := out.Close()
-    if err != nil { 
+	_, err = io.Copy(out, in)
+	cerr := out.Close()
+	if err != nil {
 		return err
 	}
 
-    return cerr
+	return cerr
 }
 
-func createFitsFile(inputFileName string, w io.Writer, test *Test) (convert.TestFile, error) {
+func convertFileToFits(inputFileName string, w io.Writer, test *Test) (convert.TestFile, error) {
 	var result convert.TestFile
 
 	// Determine the file type
 	fileType, err := convert.FileType(inputFileName)
 	if err != nil {
 		return result, fmt.Errorf("unable to determine the type of file \"%s\": %v",
-				                  inputFileName, err)
+			inputFileName, err)
 	}
 
 	// From the file type, pick the function to be called in order to convert the file
@@ -90,17 +90,17 @@ func createFitsFile(inputFileName string, w io.Writer, test *Test) (convert.Test
 	conversionFn, ok := convFunctions[fileType]
 	if !ok {
 		return result, fmt.Errorf("unsupported file type \"%s\", update stdb to the latest version",
-					              fileType)
+			fileType)
 	}
 
-	fitshdr := []fitsio.Card {
-		{ Name: "shortnam", Value: test.ShortName, Comment: "Short name of the test" },
-		{ Name: "creadate", Value: test.CreationDate, Comment: "Creation date (UTC)" },
-		{ Name: "username", Value: test.Username, Comment: "Username of the uploader" },
-		{ Name: "testtype", Value: test.TestType, Comment: "Type of the test" },
-		{ Name: "cryo", Value: test.CryogenicFlag, Comment: "Was the test done at cryogenic temperatures?" },
-		{ Name: "polarim", Value: test.Polarimeter, Comment: "Number of the polarimeter being tested" },
-		{ Name: "stdbver", Value: DatabaseSchemaVersion, Comment: "Version of the database schema" },
+	fitshdr := []fitsio.Card{
+		{Name: "shortnam", Value: test.ShortName, Comment: "Short name of the test"},
+		{Name: "creadate", Value: test.CreationDate, Comment: "Creation date (UTC)"},
+		{Name: "username", Value: test.Username, Comment: "Username of the uploader"},
+		{Name: "testtype", Value: test.TestType, Comment: "Type of the test"},
+		{Name: "cryo", Value: test.CryogenicFlag, Comment: "Was the test done at cryogenic temperatures?"},
+		{Name: "polarim", Value: test.Polarimeter, Comment: "Number of the polarimeter being tested"},
+		{Name: "stdbver", Value: DatabaseSchemaVersion, Comment: "Version of the database schema"},
 	}
 
 	// Create the FITS file
@@ -113,9 +113,9 @@ func createFitsFile(inputFileName string, w io.Writer, test *Test) (convert.Test
 // completion of this function. The return value contains the
 // unique id of the test and an Error object.
 func (conn *Connection) AddTest(newTest *Test,
-                                username string,
-								inputFileName string) (int, error) {
-	if (! conn.Active) {
+	username string,
+	inputFileName string) (int, error) {
+	if !conn.Active {
 		return -1, fmt.Errorf(MsgInactiveConnection)
 	}
 
@@ -162,7 +162,7 @@ values (?, ?, ?, ?, ?, ?, ?, ?)`,
 		return -1, err
 	}
 
-	testFile, err := createFitsFile(inputFileName, outFits, newTest)
+	testFile, err := convertFileToFits(inputFileName, outFits, newTest)
 	if err != nil {
 		outFits.Close()
 		os.Remove(outFitsFilePath)
@@ -208,12 +208,12 @@ where test_id = ?`,
 // order, that is, from the most recent test to the most ancient one. The "username"
 // is used only for logging purposes.
 func (conn *Connection) GetListOfTestIDs(username string, maxNum int) ([]int, error) {
-	if (! conn.Active) {
+	if !conn.Active {
 		return []int{}, fmt.Errorf(MsgInactiveConnection)
 	}
 
-	rows, err := conn.Connection.Query(`select test_id from tests order by test_id desc limit ?`, 
-	                                   maxNum)
+	rows, err := conn.Connection.Query(`select test_id from tests order by test_id desc limit ?`,
+		maxNum)
 	if err != nil {
 		return []int{}, err
 	}
@@ -229,27 +229,26 @@ func (conn *Connection) GetListOfTestIDs(username string, maxNum int) ([]int, er
 	}
 
 	conn.Log(fmt.Sprintf("querying the IDs of the tests in the database, %d results returned (maxNum=%d)",
-	         len(result), maxNum), username)
+		len(result), maxNum), username)
 	return result, nil
 }
-
 
 // GetTest searches for a test with the given ID in the database.
 // If a matching test is found in the database, the function fills
 // the structure pointed by "test." The parameter "username" is
 // used only for logging purposes, and it can be empty
 func (conn *Connection) GetTest(testID int,
-                                username string,
-								test *Test) error {
-	if (! conn.Active) {
+	username string,
+	test *Test) error {
+	if !conn.Active {
 		return fmt.Errorf(MsgInactiveConnection)
 	}
 
 	var (
-		shortName sql.NullString
-		description sql.NullString
+		shortName    sql.NullString
+		description  sql.NullString
 		creationDate sql.NullString
-		timeSpanSec sql.NullFloat64
+		timeSpanSec  sql.NullFloat64
 	)
 	err := conn.Connection.QueryRow(`
 select short_name,
@@ -263,15 +262,15 @@ select short_name,
 	   num_of_samples
 from tests where test_id = ?`,
 		testID).Scan(
-			&shortName,
-			&description,
-			&creationDate,
-			&test.Username,
-			&test.TestType,
-			&timeSpanSec,
-			&test.CryogenicFlag,
-			&test.Polarimeter,
-			&test.NumOfSamples)
+		&shortName,
+		&description,
+		&creationDate,
+		&test.Username,
+		&test.TestType,
+		&timeSpanSec,
+		&test.CryogenicFlag,
+		&test.Polarimeter,
+		&test.NumOfSamples)
 	if err != nil {
 		return err
 	}
